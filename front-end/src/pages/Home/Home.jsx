ce0f6412ef2480927bda60ac4868a73dc0529dda
tim <timour1712@gmail.com>
2024-06-15 16:58:55 +0200

    update home component

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import bankTree from '../../assets/bank-tree.jpeg'
 export function Home() {
     return (
         <main>
-             <Banner src={bankTree} alt="Bord de mer" content="Bank tree funny how money tree grows instead of money being devalued" />
+             <Banner src={bankTree} alt="Bank tree" content="Bank tree funny how money tree grows instead of money being devalued" />
 
         </main>
     )
