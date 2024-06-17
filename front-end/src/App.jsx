26eb156db781b3e2f1fb4353151dea3b7d82be9b
tim <timour1712@gmail.com>
2024-06-17 11:00:31 +0200

    path/route fixed ig

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import User from './pages/User/User'
+import SignIn from './pages/SignIn/SignIn'
+
+
 import './App.css'
 
 function App() {
@@ -9,7 +16,7 @@ function App() {
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/sign-in" element={<Sign-in />} />
+            <Route path="/sign-in" element={<SignIn />} />
             <Route path="/user" element={<User />} />
             <Route path="*" element={<ErrorPage />} />
 
