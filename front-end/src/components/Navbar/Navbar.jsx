2667b015c96e1ca175f7962bc2450fee01b8ef77
tim <timour1712@gmail.com>
2024-06-17 11:04:02 +0200

    add nav bar?

---
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/argentBankLogo.png'; //wroks don't change slight bug out of nowhere
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
+
+function Nav() {
+  return (
+    <nav className='header'>
+      <img src={logo} alt="Logo Argent Bank" />
+      <ul>
+        <li>
+          <NavLink to="/" activeclassname="active">Accueil</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sign-in" activeclassname="active" className='aboutlink'>Sign in</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
