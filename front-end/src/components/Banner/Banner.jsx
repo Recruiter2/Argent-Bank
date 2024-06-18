f024068e302855d092568a272a65385da5b77c02
tim <timour1712@gmail.com>
2024-06-18 12:31:03 +0200

    update css

--- front-end/src/components/Banner/Banner.jsx
+++ front-end/src/components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ import "./banner.css";
 function Banner({ src, alt, content="" }) {
     return (
       <header className='banner'>
-        <img src={src} alt={alt} />
-        <p>{content}</p>
+        <img src={src} alt={alt}  />
+        <p className="sr-only">{content}</p>
       </header>
     );
   }
