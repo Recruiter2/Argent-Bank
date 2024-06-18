6f27f4406b65781d3bd151584a30112d8dc6e6c2
tim <timour1712@gmail.com>
2024-06-19 01:21:05 +0200

    finish banner

--- front-end/src/components/Banner/Banner.jsx
+++ front-end/src/components/Banner/Banner.jsx
@@ -5,7 +5,13 @@ import "./banner.css";
 function Banner({ content="" }) {
     return (
       <header className='hero'>
-        
+        <section className="hero-content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interes rates. </p>
+          <p className="text">Open a savings account with Argent Bank Today!</p>
+        </section>
         <p className="sr-only">{content}</p>
       </header>
     );
