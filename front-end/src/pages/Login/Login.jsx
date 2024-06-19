f04ea4a640915f8c74d4e6f1a7ea1b594a43a84b
tim <timour1712@gmail.com>
2024-06-19 10:18:11 +0200

    made login part 1st version

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,8 +1,32 @@
 import './login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
   return (
-    <p>Sign in</p>
+    <main className='main bg-dark'>
+               
+        <section class="sign-in-content">
+            <FontAwesomeIcon icon={faCircleUser } /> {' ' /* added space here*/} 
+
+               <h1>Sign in</h1>
+                <form>
+                  <div className='input-wrapper'><label for="username">Username</label
+                  ><input type="text" id="username" /></div>
+
+                  <div className='input-wrapper'><label for="password">Password</label
+                  ><input type="password" id="password" /></div>
+                  
+                  <div class="input-remember">
+            <input type="checkbox" id="remember-me" /><label for="remember-me"
+              >Remember me</label
+            >
+          </div>
+                  <button class="sign-in-button">Sign In</button>
+                </form>
+  
+               </section>
+          </main>
   );
 };
 
