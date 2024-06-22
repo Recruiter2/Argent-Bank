e3f08cb6a7a4f82bde9b53d7e92c5158f71bb595
tim <timour1712@gmail.com>
2024-06-22 14:59:10 +0200

    import navbar and footer

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import User from './pages/User/User'
