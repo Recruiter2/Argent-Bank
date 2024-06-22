1e4e0d893fc4bd58ac7f14832063c8f77c3e4d46
tim <timour1712@gmail.com>
2024-06-22 15:01:48 +0200

    add bool to check if connected

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
@@ -8,16 +8,20 @@ import User from './pages/User/User'
 import Login from './pages/Login/Login'
 
 
+
 import './App.css'
 
 function App() {
+    const isConnected = useSelector((state) => state.auth.isConnected);
   return (
     <BrowserRouter>
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/user" element={<User />} />
+            <Route 
+            path="/user"
+             element={isConnected ? <User /> : <Navigate to="/login"/>} />
             <Route path="*" element={<ErrorPage />} />
 
         </Routes>
