341dbf137f488c4182accfd9d98271ecfc6c29ca
tim <timour1712@gmail.com>
2024-06-24 14:12:56 +0200

    fix for to  htmlFor= in react

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -11,14 +11,14 @@ export function Login() {
 
                <h1>Sign in</h1>
                 <form>
-                  <div className='input-wrapper'><label for="username">Username</label
+                  <div className='input-wrapper'><label htmlFor="username">Username</label
                   ><input type="text" id="username" /></div>
 
-                  <div className='input-wrapper'><label for="password">Password</label
+                  <div className='input-wrapper'><label htmlFor="password">Password</label
                   ><input type="password" id="password" /></div>
                   
                   <div className="input-remember">
-                  <input type="checkbox" id="remember-me" /><label for="remember-me"
+                  <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
                   >Remember me </label>
           </div>
                   <button className="sign-in-button">Sign In</button>
