3872c810c5e5afead9f057e2c5f5f328876dd7b5
Tim <timour1712@gmail.com>
2025-01-10 23:53:00 +0100

    add log out it's made out of : add dispatch ad link add retriever and user name notifier and switch between sign out and login depending on state of connexion (identified or not)

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import logo from '../../assets/argentBankLogo.png'; //wroks don't change slight bug out of nowhere
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { logOut } from "../../../redux/auth.action";
+import {faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 import './navbar.css';
 
 function Nav() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isConnected = useSelector((state) => state.auth.isConnected);
+  const user = useSelector((state) => state.auth.user);
+
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    dispatch(logOut());
+    navigate("/login");
+  };
   return (
     <nav className='main-nav'>
       
@@ -15,15 +30,37 @@ function Nav() {
             <h1 className='sr-only'>Argent Bank</h1>
           </NavLink>
           
-          <div>
+          {!isConnected ? (
+            <div>
             <NavLink to="/login" className='main-nav-item'>
             <FontAwesomeIcon icon={faCircleUser } /> {' ' /* added space here*/} 
              Sign in</NavLink>
           </div>
+          ) : (
+            <div className="nav-list">
+
+            <NavLink 
+              className="nav-item" 
+              to={"/profile"}>
+                <FontAwesomeIcon icon={faCircleUser} />
+              <p>{user.userName}</p>
+            </NavLink>
+
+            <NavLink 
+              className="nav-item" 
+              to={"/"} 
+              onClick={handleLogout}>
+                <FontAwesomeIcon icon={faRightFromBracket} />
+              <p>Sign Out</p>
+            </NavLink>
+
+            </div>
+          )}
+          
          
    
     </nav>
   );
-};
+}
 
 export default Nav;
