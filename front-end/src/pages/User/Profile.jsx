d65fa9aede1c1f549ddfd549b964c03ae8278ada
Tim <timour1712@gmail.com>
2025-01-13 22:59:45 +0100

    call of the acc bloc to generate the acc checkings and balance

--- front-end/src/pages/User/Profile.jsx
+++ front-end/src/pages/User/Profile.jsx
@@ -99,9 +99,16 @@ const User = () => {
       )}
       
       <h2 className="sr-only">Accounts</h2>
-      <Account_n title="Argent Bank Checking (x8349)" amount = "$2,082.79" status = "Available Balance" />
-      <Account_n title="Argent Bank Savings (x6712)" amount = "$10,928.42" status = "Available Balance" />
-      <Account_n title="Argent Bank Credit Card (x8349)" amount = "$184.30" status = "Current Balance" />
+      
+      {accountData.map((account, index) => (
+        <Account_n
+          key={index}
+          title={account.title}
+          amount={account.amount}
+          description={account.description}
+          transaction={account.transaction}
+        />
+      ))}
       
     </main>
   );
