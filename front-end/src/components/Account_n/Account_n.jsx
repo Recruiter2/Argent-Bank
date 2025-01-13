6fca0c3dd679bee9fdaf476481c9314c549d8e4e
Tim <timour1712@gmail.com>
2025-01-13 22:41:29 +0100

    the very account functional bloc using the parameters from json to generate account checking balance

--- front-end/src/components/Account_n/Account_n.jsx
+++ front-end/src/components/Account_n/Account_n.jsx
@@ -1,14 +1,21 @@
-function Account_n({ title, amount, status }) {
+import { useNavigate} from 'react-router-dom';
+function Account_n({ title, amount, description, transaction }) {
+   const navigate = useNavigate();
+  
+    const redirectToErrorPage = () => {
+        // Redirect to the error page
+        navigate('/error');
+    };
     return (
         
         <section className="account">
           <div className="account-content-wrapper">
             <h3 className="account-title">{title}</h3>
             <p className="account-amount">{amount}</p>
-            <p className="account-amount-description">{status}</p>
+            <p className="account-amount-description">{description}</p>
           </div>
           <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
+            <button className="transaction-button" onClick={redirectToErrorPage}>{transaction}</button>
           </div>
         </section>
     );
